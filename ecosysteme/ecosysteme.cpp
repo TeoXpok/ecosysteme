122b39cba6547195cb59fdc1dbabdebabe422500
TeoXpok <goncalves.teo04@gmail.com>
2024-11-12 17:14:42 +0100

    colors

--- ecosysteme/ecosysteme.cpp
+++ ecosysteme/ecosysteme.cpp
@@ -4,34 +4,26 @@
 // ALVES GAMA Miguel
 // GRUMIAUX--KAPAHOU KENMOE Janelle
 // GONCALVES Teo
-#include <Capybara>
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
-void caca() {
-    cout << "wesh";
-
-
-
-    void abbbcc() {
-        cout << "eiozufoiezhfoiehzo";
-    }
-
-}
-
-
-
+//===============================================================================================================//
 
 int main()
 {
+	printf("\033[3;42;30mTexting\033[0m\b\t");
+	printf("\033[3;43;30mTexting\033[0m\t\t");
+	printf("\033[3;44;30mTexting\033[0m\t\t");
+	printf("\033[3;104;30mTexting\033[0m\t\t");
+	printf("\033[3;100;30mTexting\033[0m\n");
 
 
-
-
-
-
-    cout << "Test !\n";
-    cout << "test 2 ";
-    cout << "Test 3";
+	for (size_t i = 0; i < 20; i++)
+	{
+		for (size_t j = 0; j < 30; j++)
+		{
+			cout << "\033[1;35;102m[ ]\033[0m";
+		}
+	}
 }
