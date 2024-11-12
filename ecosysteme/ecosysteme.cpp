1b3d5e3bc1c0c9e7a24ceccc1db055be26d60e3c
Luxueur <alves.gama.miguel@gmail.com>
2024-11-12 16:00:56 +0100

    Mise à jour
    Mise à jour

--- ecosysteme/ecosysteme.cpp
+++ ecosysteme/ecosysteme.cpp
@@ -1,20 +1,37 @@
-// ecosysteme.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
+// Groupe 7
+// Projet : Ecosysteme
+// AYITE Julia
+// ALVES GAMA Miguel
+// GRUMIAUX--KAPAHOU KENMOE Janelle
+// GONCALVES Teo
+#include <Capybara>
 #include <iostream>
+
+using namespace std;
+
+void caca() {
+    cout << "wesh";
+
+
+
+    void abbbcc() {
+        cout << "eiozufoiezhfoiehzo";
+    }
+
+}
+
+
+
 
 int main()
 {
-    std::cout << "ecosysteme";
+
+
+
+
+
+
+    cout << "Test !\n";
+    cout << "test 2 ";
+    cout << "Test 3";
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
