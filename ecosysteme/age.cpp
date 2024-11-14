2f9c0544e5ec2db813e6f366ef8871adab25200a
ArelleNox <janelleroxane@gmail.com>
2024-11-14 14:32:47 +0100

    y

--- ecosysteme/age.cpp
+++ ecosysteme/age.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include<vector>
+#include <ecosysteme.cpp>
+#include <capybara.cpp>
+using namespace std;
 
 
+/*
+âge :
+    naissance (création)
+    bébé
+    adulte
+    vieux
+    mort (destruction) : vieillesse ou tué
+*/
 
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class Age {
+private:
+    int age;
+
+public:
+    Age(int a) : age(a) {
+        if (age == 0) {
+        cout << " Naissance de " << Animal.race << "." << endl;
+        }
+    }
+
+};
