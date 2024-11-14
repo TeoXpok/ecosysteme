8cd3bb1779b0abc5c6bc34bbb34a0c2fd96d193a
Mishy0485 <ayite.julia06@gmail.com>
2024-11-14 14:02:32 +0100

    chgt

--- ecosysteme/Class.cpp
+++ ecosysteme/Class.cpp
@@ -9,7 +9,7 @@ class Animal
 {
 protected:
 	// nom, sexe, age, statut, faim, soif, race : attributs
-	string name;
+	string name; // a voir si gardé ou non
 	string race;
 	string sexe;
 	string pos;
@@ -81,16 +81,21 @@ public:
 	void reproduction(Animal& target) override
 	{
 		// trouver un animal du sexe opposé et ken
-		// création d'un nouveau bebe
+		
+		// création d'un nouveau bebe : avoir avec manager
 
 		cout << " un nouveau " << target.getRace() << " est né ! " << endl;
 	}
 
 	void hunt(Animal& target) override
 	{
-		// se deplace en fonction de la position de target
-		// si sur la meme case ou case adjacente target mouru
-
+		// se deplace en fonction de la position de target : appeler deplacement()
+		
+		// si sur la meme case mouru
+		if (x == target.getX() && y == target.getY())
+		{
+			// appel du destructeur a voir avec le manager
+		}
 		cout << name << " chasse " << target.getName() << " ! " << endl;
 
 		cout << target.getName() << " est mort, mangé par : " << name << " ! " << endl;
@@ -119,3 +124,25 @@ public:
 		}
 	}
 };
+
+/*class managerAnimal
+{
+private:
+	vector<Animal*> animaux;
+
+public:
+
+	~managerAnimal()
+	{
+		for (auto animal : animaux)
+		{
+			delete animal;
+		}
+		animaux.clear();
+	}
+
+	Animal* creerAnimal(string name, string race, string sexe, string pos, int e, int a, int h)
+	{
+		Animal* a = new Animal( name, race, sexe, pos, e, a, h)
+	}
+};*/
